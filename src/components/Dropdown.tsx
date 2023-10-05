fdf0605db2b3c8e5b0e181a87b037591279b3f28
telsabate-hub <telsabate.business@gmail.com>
2023-10-05 20:19:38 +0800

    Fixed dropdown tab, enter, and escape event listeners.

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useRef } from 'react';
 import '../styles/dropdown.css'
 
 interface DropdownContent {
@@ -13,25 +13,47 @@ function Dropdown(props: DropdownContent){
     const inputBoxRef = useRef<HTMLInputElement>(null);
     const searchBoxRef = useRef<HTMLInputElement>(null);
 
-    const showList = (): void => {
+    const toggleVisibility = (): void => {
         const elem = inputBoxRef.current;
 
         elem?.classList.toggle("open");
 
+        if( elem?.classList.contains("open") ){
+            showList();
+        } else {
+            hideList();
+        }
+    }
+
+    const showList = (): void => {
+        const elem = inputBoxRef.current;
+        const firstItem = document.getElementById('id0');
+        const firstLabel: HTMLLabelElement = firstItem?.nextElementSibling as HTMLLabelElement;
+
         let list = elem?.nextElementSibling as HTMLElement;
 
-        if( list?.style.maxHeight ) {
-            list.style.removeProperty( 'max-height' );
-            list.style.removeProperty( 'box-shadow' );
-        } else {
-            list.style.maxHeight = list.scrollHeight + "px";
-            list.style.boxShadow = "0 1px 2px 0 rgba(0, 0, 0, 0.15),0 1px 3px 1px rgba(0, 0, 0, 0.1)";
-        }
+        list.style.maxHeight = list.scrollHeight + "px";
+        list.style.boxShadow = "0 1px 2px 0 rgba(0, 0, 0, 0.15),0 1px 3px 1px rgba(0, 0, 0, 0.1)";
+
+        // firstLabel.focus();
+        firstLabel.classList.add('highlight');
+        // document.getElementById('preview')?.blur();
+        document.getElementById('search')?.focus();
+    }
+
+    const hideList = (): void => {
+        const elem = inputBoxRef.current;
+        let list = elem?.nextElementSibling as HTMLElement;
+
+        list.style.removeProperty( 'max-height' );
+        list.style.removeProperty( 'box-shadow' );
 
         //Reset the search box value
         const searchBox = document.getElementById('search') as HTMLInputElement;
         searchBox.value = "";
         searchBox.dispatchEvent( new Event('input', {bubbles: true}) );
+
+        document.getElementById('preview')?.focus();
     }
 
     const selectItem = (e: ChangeEvent): void => {
@@ -57,6 +79,34 @@ function Dropdown(props: DropdownContent){
         })
     }
 
+    const keyUpListener = (e: KeyboardEvent): void => {
+        const key: string = e.key;
+        // console.log(`key: ${key}`)
+        if( key == "Enter" ){
+            showList();
+        } else if( key == "Escape" || key == "Tab" ){
+            hideList();
+        }
+    }
+
+    const itemsListKeyUpListener = (e: KeyboardEvent): void => {
+        const key: string = e.key;
+
+        if( key == "Escape" || key == "Enter" ){
+            hideList();
+        } else if( key == "ArrowDown" ){
+            
+        }
+    }
+
+    const searchKeyUpListener = (e: KeyboardEvent): void => {
+        const key: string = e.key;
+
+        if( key == "Escape" ){
+            hideList();
+        }
+    }
+
     return (
         <>
             <div className="dropdown" style={{width: props.width || "100%"}}>
@@ -67,7 +117,10 @@ function Dropdown(props: DropdownContent){
                     ref={inputBoxRef}
                     style={{height: props.height || "40px"}} 
                     data-custom-msg={props.defaultValue} 
-                    onClick={showList}>
+                    onClick={toggleVisibility}
+                    onKeyUp={keyUpListener}
+                    tabIndex={0}
+                >
                 </div>
                 <div className='list'>
                     <div className='search-box'>
@@ -78,17 +131,20 @@ function Dropdown(props: DropdownContent){
                             placeholder="Search Options"
                             ref={searchBoxRef}
                             onInput={search}
+                            onKeyUp={searchKeyUpListener}
                         />
-                        {props.itemsList.map((element, index) => {
-                            return (
-                                <div key={'item' + index}>
-                                    <input type='radio' name='drop1' id={'id' + index} className='radio' onChange={selectItem}/>
-                                    <label htmlFor={'id' + index}>
-                                        <span className='name'>{element}</span>
-                                    </label>
-                                </div>
-                            );
-                        })}
+                        <div className='items-list' tabIndex={0} onKeyUp={itemsListKeyUpListener}>
+                            {props.itemsList.map((element, index) => {
+                                return (
+                                    <div key={'item' + index}>
+                                        <input type='radio' name='drop1' id={'id' + index} className='radio' onChange={selectItem}/>
+                                        <label htmlFor={'id' + index}>
+                                            <span className='name'>{element}</span>
+                                        </label>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
